376b37e991822beaaaee2b5cc5a8beea8907b1e5
Tyler <tyler@ubuntu.ubuntu-domain>
2014-02-21 18:25:51 -0500

    setup initial layout

--- ItemFinder/gen/com/example/itemfinder/R.java
+++ ItemFinder/gen/com/example/itemfinder/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int addItemButton=0x7f080002;
+        public static final int itemListView=0x7f080001;
+        public static final int searchEditText=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +37,12 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_item_button=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int item_list_view=0x7f050005;
+        public static final int search_edit_text_hint=0x7f050003;
+        public static final int search_submit_button=0x7f050004;
     }
     public static final class style {
         /** 
--- ItemFinder/src/com/example/itemfinder/MainActivity.java
+++ ItemFinder/src/com/example/itemfinder/MainActivity.java
@@ -1,19 +1,68 @@
 package com.example.itemfinder;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	Button btnObj;
+
+    ArrayAdapter<String> adapter;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+//        btnObj = (Button) this.findViewById(R.id.btnTESTBUTTON);
+        ListView lstView = (ListView) findViewById(R.id.itemListView);
+        
+        ArrayList<String> arrayList = new ArrayList<String>(100);
+        for(int i = 0; i < 100; i++)
+        {
+        	arrayList.add(Integer.toString(i));
+        }
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        
+        lstView.setAdapter(adapter);        
     }
 
 
+    public void testMethod(View someView)
+    {
+    	adapter.add("added");
+    }
+    
     @Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		
+	}
+
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
