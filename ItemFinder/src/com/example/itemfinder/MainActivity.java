12d7d24c4fd43c54ae1d0b21d5e01f4cc739d907
dv297 <dvv297@gmail.com>
2014-02-21 20:55:46 -0500

    ItemInfo added

--- ItemFinder/src/com/example/itemfinder/MainActivity.java
+++ ItemFinder/src/com/example/itemfinder/MainActivity.java
@@ -1,69 +1,31 @@
 package com.example.itemfinder;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import java.util.List;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
 	private ArrayAdapter<String> adapter; // Used to search for class list.
 	private ListView itemListView;
-	
-	private static DataSource dataSource;
+
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-//        btnObj = (Button) this.findViewById(R.id.btnTESTBUTTON);
-        ListView lstView = (ListView) findViewById(R.id.itemListView);
-        
-        ArrayList<String> arrayList = new ArrayList<String>(100);
-        for(int i = 0; i < 100; i++)
-        {
-        	arrayList.add(Integer.toString(i));
-        }
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
-        
-        lstView.setAdapter(adapter);        
+        setContentView(R.layout.test_layout);
     }
 
 
-    public void testMethod(View someView)
-    {
-    	adapter.add("added");
-    }
-    
     @Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		
-	}
-
-
-	@Override
-	protected void onRestart() {
-		// TODO Auto-generated method stub
-		super.onRestart();
-	}
-
-
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-	}
-
-
-	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -71,12 +33,14 @@ public class MainActivity extends Activity {
     }
     
 	public void initList(){
-		List<Item> items_list = dataSource.getAllItems();
-		ArrayList<String> items_string = new ArrayList<String>();
+		List<Item> items_list = ContentHolder.getDS().getAllItems();
+		ArrayList<String> items_string = new ArrayList<String>(100);
 		itemListView = (ListView) findViewById(R.id.itemListView);
-		if(items_list.size() != 0){
-			for(int x = 0; x<items_list.size(); x++){
-				items_string.add(items_list.get(x).getName());
+		//if(items_list.size() == 0){ // changed
+			for(int x = 0; x<100; x++){ // changed
+				//items_string.add(items_list.get(x).getName());
+				// Testing purposes
+				items_string.add("TEST");
 			}
 			adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items_string);
 			
@@ -87,8 +51,17 @@ public class MainActivity extends Activity {
 				@Override
 				public void onItemClick(AdapterView<?> parent, View arg1, int position,
 						long arg3) {
-//					//Intent i = new Intent(arg1.getContext(), GradeListActivity.class);
-//			        //final String item = (String) parent.getItemAtPosition(position);
+					Intent i = new Intent(arg1.getContext(), ItemInfoActivity.class);
+			        final String item_name = (String) parent.getItemAtPosition(position);
+			        Item item = new Item();
+			        List<Item> item_list = ContentHolder.getDS().getAllItems();
+			        for(int x = 0; x<item_list.size(); x++){
+			        	if(item_name.equals(item_list.get(x).getName())){
+			        		item = item_list.get(x);
+			        	}
+			        }
+			        
+			        ItemInfoActivity.setValues(item.getName(), item.getLocation(), item.getKeywords());
 //					//GradeListActivity.setTitle(item);
 //					//GradeListActivity.setSubject(new Subject(ContentHolder.getStudent().getName(), item, "ABSOLUTE")); // FIX THIS TEMPORARY CODE.
 //					//startActivity(i);
@@ -97,11 +70,7 @@ public class MainActivity extends Activity {
 				
 			});
 		}
-	}
-	
-	public void submitButtonClick(View view){
-		
-	}
+//	}
 	
 	public void addButtonClick(View view){
 		
